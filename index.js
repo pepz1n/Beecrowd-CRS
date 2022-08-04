43f48f9c16c8160845775865e8fe3e8b58394065
Bernardo <bebezanetti@gmail.com>
2022-08-03 21:46:49 -0300

    forget

--- index.js
+++ index.js
@@ -3,29 +3,20 @@ var lines = input.split('\n');
 
 const pegarValores = (line) => line.split(" ").map(a => Number(a));
 
-let [a, b, c, d] = pegarValores(lines.shift());
+let [a, b, c] = pegarValores(lines.shift());
 
-let media = (a*0.2) + (b*0.3) + (c*0.4) + (d*0.1) 
 
-if(media >= 7){
-  console.log(`Media: ${(Math.floor(media*100)/100).toFixed(1)}`);
-  console.log("Aluno aprovado.");
-}else if(media >= 5){
-  console.log(`Media: ${(Math.floor(media*100)/100).toFixed(1)}`);
-  console.log("Aluno em exame.");
-  let exame = Number(lines.shift())
-  console.log(`Nota do exame: ${exame.toFixed(1)}`);
-  media += exame;
-  media = media/2
-  if(media>=5){
-    console.log("Aluno aprovado.");
-    console.log(`Media final: ${(Math.floor(media*100)/100).toFixed(1)}`);
+if (a>b) {
+  if (a>c) {
+    console.log(a);
   }else{
-    
-    console.log("Aluno reprovado.");
-    console.log(`Media final: ${(Math.floor(media*100)/100).toFixed(1)}`);
+    console.log(c);
   }
-}else{
-  console.log(`Media: ${(Math.floor(media*100)/100).toFixed(1)}`);
-  console.log("Aluno reprovado.");
+  if (b>c) {
+    console.log(b);
+    console.log(c);
+  }else{
+    console.log(c);
+    console.log(b);
+  }
 }
