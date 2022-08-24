abe3bb80336cc0e51eea5a11638be5c12fb90593
pepz1n <bernardo.zanetti@unochapeco.edu.br>
2022-08-24 18:26:02 -0300

    forget

--- Beecrowd/resoluções/2060.js
+++ Beecrowd/resoluções/2060.js
@@ -1,15 +1,13 @@
-var input = require('fs').readFileSync('./Beecrowd/dev/stdin.txt', 'utf8');
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
 var lines = input.split('\n');
-
-// const pegarValores = (line) => line.split(" ").map(a => Number(a));
 const pegarValores = (line) => line.split(" ").map(a => Number(a));
 let a = lines.shift()
 let b = pegarValores(lines.shift())
 
-let multiplo2 = 0 -1
-let multiplo3 = 0 -1
-let multiplo4 = 0 -1
-let multiplo5 = 0 -1
+let multiplo2 = 0 
+let multiplo3 = 0 
+let multiplo4 = 0
+let multiplo5 = 0 
 
 
 
